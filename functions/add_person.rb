be23cd7b8e50ba93cada90b87ae8400ba7210679
zkr024 <zkr.bm.02403@gmail.com>
2022-06-27 18:51:10 -0500

    Correct linter errors

--- functions/add_person.rb
+++ functions/add_person.rb
@@ -1,6 +1,7 @@
 require_relative '../school/student'
 require_relative '../school/teacher'
 require_relative '../menu/valid'
+require_relative './person_options'
 
 class AddPerson
   def add_person(person)
@@ -9,29 +10,11 @@ class AddPerson
     if valid_person?(selection)
       case selection
       when '1'
-        print 'Age: '
-        age = gets.chomp
-        print 'Name: '
-        name = gets.chomp
-        print 'Classroom: '
-        classroom = gets.chomp
-        print 'Has parent permission? [Y/N]: '
-        permission = gets.chomp
-        loop do
-          break if permission.capitalize == 'N' || permission.capitalize == 'Y'
-
-          puts "#{permission} is not a valid entry, try again"
-          permission = gets.chomp
-        end
-        person_data = Student.new(classroom, age, name)
+        option_one
+        person_data = Student.new(@classroom, @age, @name)
       when '2'
-        print 'Age: '
-        age = gets.chomp
-        print 'Name: '
-        name = gets.chomp
-        print 'Specialization: '
-        specialization = gets.chomp
-        person_data = Teacher.new(specialization, age, name)
+        option_two
+        person_data = Teacher.new(@specialization, @age, @name)
       end
     else
       puts "option #{@input} is invalid, please try again"
--- functions/rental_list.rb
+++ functions/rental_list.rb
@@ -1,27 +1,14 @@
+require_relative './rent_available'
 class RentalList
   def rental_list(rental, person)
     if rental.empty?
       puts 'There are no rentals'
     else
-      print "person's ID: "
-      id = gets.chomp.to_i
-      record = person.filter_map do |i|
-        i.name if i.id == id
-      end
-      loop do
-        break if record.length.positive? || id.zero?
-
-        puts "Invalid ID #{id}, please try again Or go back to the main menu by pressin 0"
-        id = gets.chomp.to_i
-        record = person.filter_map do |i|
-          i.name if i.id == id
-        end
-      end
-      value = record.join
+      rent_available(person)
     end
-    puts "Name: #{value}"
+    puts "Name: #{@value}"
     (rental.filter_map do |res|
-      puts "Date: #{res.date}, Book '#{res.book.title}' by #{res.book.author}" if res.person.name == value
+      puts "Date: #{res.date}, Book '#{res.book.title}' by #{res.book.author}" if res.person.name == @value
     end)
   end
 end
